0c40beb8f0dd3c7835530a11a76a7236dc8f2e6b
nathan26036 <nathandickinson04@gmail.com>
2024-02-06 18:07:54 -0500

    created models for the blog and comments and add contollers to give functionality and created many veiws for each page such as login sign up or the dashboard

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../../config/connection');
+const { User, Post, Comment} = require('../models');
+
+const userData = require('./userData.json');
+const blogData = require('./blogData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of blogData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
